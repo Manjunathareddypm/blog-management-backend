23f06ee4c91a5c873379d3b7d5c7fca1157394c0
mreddypm@gmail.com <mreddypm@gmail.com>
2023-04-20 13:30:31 +0530

    [Manjunatha]added:service.js

--- project/src/services/user.service.js
+++ project/src/services/user.service.js
@@ -1,4 +1,8 @@
 import User from '../models/user.model';
+import HttpStatus from 'http-status-codes';
+const bcrypt = require('bcrypt');
+import jwt from "jsonwebtoken";
+import { error } from '@hapi/joi/lib/base';
 
 
 export const getAllUsers = async () => {
@@ -6,30 +10,45 @@ export const getAllUsers = async () => {
   return data;
 };
 
-export const newUser = async (body) => {
+//create new user
+export const newRegistration = async (body) => {
+  var stat;
+  const res = await User.findOne({email: body.email})
+  if(res == null ){
+  const saltRounds = 10;
+  const hashpassword = await bcrypt.hash(body.password, saltRounds);
+  body.password = hashpassword;
   const data = await User.create(body);
-  return data;
-};
-
-export const updateUser = async (_id, body) => {
-  const data = await User.findByIdAndUpdate(
-    {
-      _id
-    },
-    body,
-    {
-      new: true
+  stat = {
+    code: HttpStatus.CREATED,
+    data: data,
+    message: 'User Registered successfully'
+  }
+  }else{
+    stat = {
+      code: HttpStatus.BAD_REQUEST,
+      data: "data",
+      message: 'User already registered'
     }
-  );
-  return data;
+  }
+  return stat
 };
 
-export const deleteUser = async (id) => {
-  await User.findByIdAndDelete(id);
-  return '';
-};
 
-export const getUser = async (id) => {
-  const data = await User.findById(id);
-  return data;
-};
+//user Login
+
+export const userLogin = async (body) => {
+  var token;
+  const data = await User.findOne({email: body.email})
+  if(data){
+    const pswd = await bcrypt.compare(body.password, data.password);
+    if (!pswd) {
+      throw new error("enter correct password")
+    }else 
+     token= jwt.sign({email:data.email, id:data._id},
+      process.env.SECRET_KEY);
+      data.token = token
+      return data.token
+     }
+  };
+
